2b34fa39aa1bea112bfc618befadaeb14b1fa153
Dimitar Yurukov <mscalindt@protonmail.com>
2019-07-09 22:06:27 +0300

    tree: Rework
    Rework for Nano kernel.
    
    Features:
    
    - FKKM profiles.
    - Nano updater script.
    
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME ?= Nano Kernel
+
+DATE := $(shell date "+%Y%m%d-%H%M")
+
+CODE := Kernel-rosy
+
+ZIP := $(NAME)-$(CODE)-$(DATE).zip
+
+EXCLUDE := Makefile *.git* *.jar* *placeholder* *.md*
+
+normal: $(ZIP)
+
+$(ZIP):
+	@echo "Creating ZIP: $(ZIP)"
+	@zip -r9 "$@" . -x $(EXCLUDE)
+	@echo "Generating SHA1..."
+	@sha1sum "$@" > "$@.sha1"
+	@cat "$@.sha1"
+	@echo "Done."
+
+
+clean:
+	@rm -vf *.zip*
+	@rm -vf zImage
+	@echo "Done."
+
+
--- anykernel.sh
+++ anykernel.sh
@@ -4,35 +4,29 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Nano kernel for Redmi 5
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+do.cleanuponabort=1
+device.name1=rosy
+device.name2=Redmi 5
+supported.versions=7.1.2 - 9
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
-
 
 ## AnyKernel install
 dump_boot;
@@ -40,23 +34,15 @@ dump_boot;
 # begin ramdisk changes
 
 # init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+insert_line init.rc "import /init.nano.rc" after "import /init.trace.rc" "import /init.nano.rc";
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# sepolicy
+$bin/magiskpolicy --load sepolicy --save sepolicy \
+  "allow init rootfs file execute_no_trans" \
+;
 
 # end ramdisk changes
 
 write_boot;
-## end install
 
+## end install
